811c06be648dd9482d78ea8f81d934a6c22dbc60
kkron4221 <knroro1877887@gmail.com>
2020-08-12 08:20:05 +0900

    Connect form and views.py

---
+++ mysite/food_list/test.py
@@ -0,0 +1,7 @@
+from django.test import TestCase
+
+from .models import InputForm
+
+class Tests()：
+  def string_test(self):
+    
--- mysite/food_list/views.py
+++ mysite/food_list/views.py
@@ -1,14 +1,21 @@
-from django.shortcuts import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.shortcuts import render
-from .models import Title
+from django.http import Http404
+from django.urls import reverse
+
 
 
 def index(request):
   return render(request, 'food_list/index.html')
   
+# search material from api
 def search(request):
-  return render(request, 'food_list/result.html')
+  return HttpResponse("hogehoge")
 
 def results(request):
-  return render(request, 'food_list/result.html')
+  params = {
+    'food_name' : request.POST.get('search'),
+    'material' : '卵',
+  }
+  return render(request, 'food_list/result.html', params)
