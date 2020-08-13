b96e9948e1413ddb98c54c319874a0d9b13d8dc3
kkron4221 <knroro1877887@gmail.com>
2020-08-13 10:26:34 +0900

    Connect selenium check test

--- mysite/food_list/test.py
+++ mysite/food_list/test.py
@@ -1,5 +1,10 @@
 from django.test import TestCase
 
 class Tests()：
-  def string_test(self):
-    
+  def string_test_correct(self):
+    input_date = "ハンバーグ"
+    self.assertIs(, False)    
+
+  def string_test_wrong(self):
+    input_date = "hogehoge"
+    self.assertIs(, False)
--- mysite/food_list/views.py
+++ mysite/food_list/views.py
@@ -1,15 +1,34 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import loader
+from selenium import webdriver
+import time
 
 def index(request):
-  num = list(range(10))
-  context = {'num_list' : num}
-  return HttpResponse(render(request, 'food_list/index.html', context))
+  return HttpResponse(render(request, 'food_list/index.html'))
   
 def results(request):
-  params = {
-    'food_name' : request.POST.get('search'),
-    'material' : '卵',
-  }
-  return render(request, 'food_list/result.html', params)
+  food_name = request.POST.get('search1')
+  url = "https://www.kurashiru.com/"
+
+  driver = webdriver.Firefox()
+  driver.get(url)
+  search_box = driver.find_element_by_id("search_box")
+  search_box.send_keys(Keys.ENTER, food_name)
+  time.sleep(2)
+
+  # item = driver.find_element_by_class_name("dly-video-item-root[1]")
+  # item.click()
+  # time.sleep(2)
+
+  # materials = driver.find_element_by_class_name("ingredient-list-item")
+
+  # return HttpResponse(materials)
+
+  return HttpResponse(food_name)
+
+  # params = {
+  #   'food_name' : food_name,
+  #   'material' : '卵',
+  # }
+  # return render(request, 'food_list/result.html', params)
