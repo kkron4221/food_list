a1de385959f1d34c2bb5e4775dfee5a52b7b776a
ricky <knroro1877887@gmail.com>
2020-08-18 08:15:14 +0900

    Separate selenium function

---
+++ mysite/food_list/search_material.py
@@ -0,0 +1,30 @@
+from django.http import HttpResponse
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.keys import Keys
+import time
+
+def selenium_function(food_name):
+    url = "https://www.kurashiru.com/"
+
+    options = Options()
+    options.add_argument("-headless")
+
+    driver = webdriver.Firefox(firefox_options=options)
+    driver.get(url)
+    time.sleep(2)
+    search_box = driver.find_element_by_id("query")
+    search_box.send_keys(food_name)
+    search_box.send_keys(Keys.ENTER)
+    time.sleep(2)
+
+    items = driver.find_elements_by_class_name("dly-video-item-root")
+    items[1].click()
+    time.sleep(2)
+
+    materials = driver.find_elements_by_class_name("ingredient-list-item")
+    material_list = []
+    for i in range(len(materials)):
+        material_list.append(materials[i].text.splitlines())
+
+    return material_list
