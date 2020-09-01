c87a62765bebed21baf8b9ab746981b32e6c76b4
ricky <knroro1877887@gmail.com>
2020-09-01 21:11:53 +0900

    Fixed

--- mysite/food_list/create_table.py
+++ mysite/food_list/create_table.py
@@ -1,36 +1,17 @@
 from food_list.models import FoodInfo
 import re
 
-# TOOD:delete table colum
-
 def insert_colum(food_name, material_list):
     for i in range(len(material_list)):
         try:
-<<<<<<< HEAD
-            material_list[i][1]  = str(material_list[i][1])
+            material = material_list[i][0]
             amount = re.sub('\\D', "", material_list[i][1])
+            amount = int(amount)
             unit = re.sub('[0-9０-９]+', "", material_list[i][1])
-            if unit == "大さじ":
-                unit = "小さじ"
-                amount = int(amount) * 3
-            if unit == "/本":
-                unit = "個"
-            if unit == "合":
-                unit = "150g"
-=======
-            material = str(material_list[i][0])
-            amount = re.sub(r"\D", "", material_list[i][1])
-            unit = re.sub(r'\d', "", material_list[i][1])
->>>>>>> 0036a2876ac30d53015612291e0a7be03fb6d1ed
         except:
             ValueError
             continue
-        try:
-            table_row = FoodInfo.objects.create(food_name=food_name, material=material, amount=amount, unit=unit)
-            table_row.save()
-        except:
-            ValueError
-            continue
+        table_row = FoodInfo.objects.create(food_name=food_name, material=material, amount=amount, unit=unit)
 
 # TODO:sort and add table colum
     
--- mysite/food_list/views.py
+++ mysite/food_list/views.py
@@ -11,7 +11,6 @@ def index(request):
 def results(request):
   FoodInfo.objects.all().delete()
   foods_list = []
-  materials_list = []
   url_list = []
 
   for i in range(10):
@@ -25,29 +24,13 @@ def results(request):
     material_list = food_selenium[0]
     foods_url = food_selenium[1]
     create_table.insert_colum(food_name, material_list)
-    materials_list.append(material_list)
     days = i
-<<<<<<< HEAD
 
   result_info = FoodInfo.objects.values('material', 'unit').order_by('material').annotate(amount=Sum('amount'))
 
   context = {
             'result_info':result_info,
             'foods_list':foods_list,
-=======
-  
-  allfoods = FoodInfo.objects.all()
-  sorted_foods_material = sorted(allfoods, key=lambda FoodInfo:FoodInfo.material)  
-
-  context = {
-            'allfoods':allfoods,
-            'sorted_foods_material':sorted_foods_material,
->>>>>>> 0036a2876ac30d53015612291e0a7be03fb6d1ed
             'days':days,
             }
-
-<<<<<<< HEAD
-=======
-
->>>>>>> 0036a2876ac30d53015612291e0a7be03fb6d1ed
   return render(request, 'food_list/result.html', context)
